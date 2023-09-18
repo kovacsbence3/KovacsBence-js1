60046b644beff3dddbb7b2acdf1b0302e396f304
Kovács Bence <hobbemail0@gmail.com>
2023-09-18 16:30:24 +0200

    Elso feladat

---
+++ feladat03.js
@@ -0,0 +1,5 @@
+function listaKiiras(){
+    const lista = [0, 7, 16, 30, 41];   // lista elkészítése
+    const revlist = lista.reverse();    // lista megforítása
+    console.log(revlist);               // lista kiírása
+}
